8aaff325a2d4669190c4254e7eb23580929ece5a
MainShayne233 <shaynetremblay@gmail.com>
2019-09-02 01:14:34 -0400

    Parse nonterminals!

---
+++ src/ast.rs
@@ -0,0 +1,4 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Grammar {
+    pub nonterminals: Vec<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate lalrpop_util;
+
+use crate::ast::Grammar;
+
+lalrpop_mod!(pub grammar);
+
+pub mod ast;
+
+#[test]
+fn test_nonterminals_parser() {
+    let input = r#"
+Nonterminals
+  grammar expr_list
+  expr container_expr block_expr access_expr
+  call_args_parens_expr call_args_parens_base call_args_parens parens_call
+  number
+  .
+"#;
+
+    let program = grammar::GrammarParser::new().parse(input).unwrap();
+    assert_eq!(
+        program,
+        Grammar {
+            nonterminals: vec![
+                String::from("grammar"),
+                String::from("expr_list"),
+                String::from("expr"),
+                String::from("container_expr"),
+                String::from("block_expr"),
+                String::from("access_expr"),
+                String::from("call_args_parens_expr"),
+                String::from("call_args_parens_base"),
+                String::from("call_args_parens"),
+                String::from("parens_call"),
+                String::from("number")
+            ]
+        }
+    )
 }
