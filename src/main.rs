f227738a0eb40f204425612fb5f547b277581577
MainShayne233 <shaynetremblay@gmail.com>
2019-09-02 21:02:17 -0400

    Finish declarations parsing!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lalrpop_util;
 
-use crate::ast::{Grammar, Declaration, DeclarationType};
+use crate::ast::{Declaration, DeclarationType, Grammar};
 use crate::preprocess::*;
 
 lalrpop_mod!(pub grammar);
@@ -10,7 +10,9 @@ pub mod ast;
 pub mod preprocess;
 
 fn parse_grammar(input: &str) -> Grammar {
-    grammar::GrammarParser::new().parse(&strip_extra(input)).unwrap()
+    grammar::GrammarParser::new()
+        .parse(&strip_extra(input))
+        .unwrap()
 }
 
 #[test]
@@ -33,6 +35,12 @@ Rootsymbol grammar. % sample past code comment
 %% Two shift/reduce conflicts coming from call_args_parens and
 %% one coming from empty_paren on stab.
 Expect 3.
+
+Left       5 do.
+Right     10 stab_op_eol.     %% ->
+Left      20 ','.
+Left      40 in_match_op_eol. %% <-, \\ (allowed in matches along =)
+Nonassoc 300 unary_op_eol.    %% +, -, !, ^, not, ~~~
 "#;
 
     let grammar = parse_grammar(input);
@@ -79,6 +87,26 @@ Expect 3.
             ),
             Declaration(DeclarationType::Rootsymbol, vec![String::from("grammar")]),
             Declaration(DeclarationType::Expect, vec![String::from("3")]),
+            Declaration(
+                DeclarationType::Left,
+                vec![String::from("5"), String::from("do")]
+            ),
+            Declaration(
+                DeclarationType::Right,
+                vec![String::from("10"), String::from("stab_op_eol")]
+            ),
+            Declaration(
+                DeclarationType::Left,
+                vec![String::from("20"), String::from("','")]
+            ),
+            Declaration(
+                DeclarationType::Left,
+                vec![String::from("40"), String::from("in_match_op_eol")]
+            ),
+            Declaration(
+                DeclarationType::Nonassoc,
+                vec![String::from("300"), String::from("unary_op_eol")]
+            ),
         ]
     )
 }
