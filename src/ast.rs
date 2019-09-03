06f6cef883e542b3ae2632f30abfc26ff8294a0e
MainShayne233 <shaynetremblay@gmail.com>
2019-09-02 21:52:55 -0400

    First go at grammar parser

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,35 @@
 #[derive(Debug, Clone, PartialEq)]
 pub struct Grammar {
     pub declarations: Vec<Declaration>,
+    pub nodes: Vec<Node>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Node {
+    pub lhs: String,
+    pub rhs: Vec<String>,
+    pub expression: NodeExpression,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum NodeExpression {
+    Charlist {
+        value: String,
+    },
+    Tuple {
+        values: Box<Vec<NodeExpression>>,
+    },
+    FunctionCall {
+        name: String,
+        args: Box<Vec<NodeExpression>>,
+    },
+    List {
+        values: Box<Vec<NodeExpression>>,
+    },
+    HeadTailList {
+        head: Box<Vec<NodeExpression>>,
+        tail: Box<NodeExpression>,
+    },
 }
 
 #[derive(Debug, Clone, PartialEq)]
